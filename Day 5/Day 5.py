f185b463e11abe08ffc6236052e9b2fa318181d8
Goh Xue Zhe <gohxuezhe@gmail.com>
2023-12-05 20:36:25 +0800

    Day 5, no idea for part 2

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -1,22 +1,47 @@
 import sys
+from functools import reduce
 
-def main1(arr):
-    res = 0
+### Credit: https://topaz.github.io/paste/#XQAAAQC4AwAAAAAAAAA5mWzIdkeB3YAaFzvndN+LqCZhkL+YJtr++JeuPCpp7fxlnbBaB4dZQGuOmX73Qjw5CE9+deVMOb4jP/Xw1FZpFYdeR7AXmGyhVlmyJYdzT+9u0bbzBbcRcr3K+kWrkNxsqwVwnC26G4zs4+t5pSg7vXlkWYALNHP/F8aG4yhYz6TkhEoY1zR3gzHfubbCw2JXPrPZzrHuOiEiyCWvzXR0dF4pYTN09kWF1ymgnQDGJ3BleiMcbJH4sUXzmtyayvGq0LyhK/FKS6rC6c8CfA2biK3tWo3B6cX67tCOp3RixETsmjvJjs/mPoeSoqMnu3V7EGxGib8m8PK7nJ60gW4Iy9M2hKwNuwx7if80RqFNie67Z9HXUuXNy+0HCW0oDcFUu6J4Ht4oDNyxYuuAl7ocTz4A4SDwKgJsyza1g72jHAsFGwkOMImDsB9ktQ+svRQqB2VRiDrU+iEReRVZqnOfqoyApVAhahiaEQjgVUJvzciGovxrKFO4P3HHHQHQ9ihgjG+txn6amcc0e27ooRZphM+/Gn9eOZ/ao2W66OcwNhpECRQK/TkIkw==
 
-    return res
+def main1(seeds, maps):
+    def mapping(seed, _map):
+        for output_start, input_start, length in _map:
+            if input_start <= seed <= input_start + length:
+                return output_start + seed - input_start
+        return seed
+    return min(reduce(mapping, maps, seed) for seed in seeds)
 
-def main2(arr):
-    res = 0
-
-    return res
+def main2(seeds, maps):
+    start_lengths = list(zip(seeds[::2], seeds[1::2]))
+    
+    for m in maps:
+        print(start_lengths)
+        new_start_lengths = []
+        for seed_start, seed_length in start_lengths:
+            for output_start, input_start, map_length in m:
+                if seed_start < input_start + map_length and input_start < seed_start + seed_length:
+                    new_start = max(seed_start, input_start)
+                    new_length = min(seed_start + seed_length, input_start + map_length) - new_start
+                    new_start_lengths.append((new_start - input_start + output_start, new_length))
+                    if new_start > seed_start:
+                        start_lengths += [(seed_start, new_start - seed_start)]
+                    if new_start + new_length < seed_start + seed_length:
+                        start_lengths.append((new_start + new_length, seed_start + seed_length - new_start - new_length))
+                    break
+            else:
+                new_start_lengths.append((seed_start, seed_length))
+        start_lengths = new_start_lengths
+    return (min(r for r, _ in start_lengths))
 
 def read_file(file_path):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
-            content = content.split('\n')
-            print("Part 1:", main1(content))
-            print("Part 2:", main2(content))
+            seeds, *maps = content.split('\n\n')
+            seeds = [int(seed) for seed in seeds.split()[1:]]
+            maps = [[[int(i) for i in line.split()] for line in map.splitlines()[1:]] for map in maps]
+            print("Part 1:", main1(seeds, maps))
+            print("Part 2:", main2(seeds, maps))
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
     except Exception as e:
